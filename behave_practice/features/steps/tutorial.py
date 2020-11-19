f860c1938b44138d2749423ad8a78e7145b45a76
yumechi <yumechi0525@gmail.com>
2020-11-20 05:04:39 +0900

    チュートリアルシナリオをコピペして少し改変する

---
+++ behave_practice/features/steps/tutorial.py
@@ -0,0 +1,21 @@
+from behave import given, when, then
+
+
+@given("we have behave installed")
+def installed(context):
+    pass
+
+
+@when("we implement a test")
+def a_test(context):
+    assert True is not False
+
+
+@then("behave will test it for us!")
+def it_for_us(context):
+    assert context.failed is False
+
+
+@step("we enjoy test!")
+def enjoy_test(context):
+    assert 42 is not False
---
+++ behave_practice/features/tutorial.feature
@@ -0,0 +1,9 @@
+Feature: showing off behave
+  It is sample.
+  Sample Scenario.
+
+  Scenario: run a simple test
+     Given we have behave installed
+      When we implement a test
+      Then behave will test it for us!
+       And we enjoy test!
