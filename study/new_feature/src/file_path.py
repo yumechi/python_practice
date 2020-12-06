95f80a46b4d3143ae60daf1e21c41aaac813ecf6
yumechi <yumechi0525@gmail.com>
2020-12-06 17:37:58 +0900

    get __file__ path

---
+++ study/new_feature/src/file_path.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    import os
+    current_file_path = f"{os.getcwd()}/src/file_path.py"
+    print(__file__)
+    print(current_file_path)
+    assert __file__ == current_file_path
