735341abfa50aecd6654446af01a741e90bca3e3
yumechi <yumechi0525@gmail.com>
2020-12-07 14:58:51 +0900

    named tuple test

---
+++ study/new_feature/src/namedtuple_sample.py
@@ -0,0 +1,24 @@
+from typing import NamedTuple
+
+
+class Idol(NamedTuple):
+    name: str
+    profile: str
+
+
+class Live(NamedTuple):
+    title: str
+    member: list[Idol]
+
+    def summary(self) -> str:
+        return "title: {title}\nmember: \n  - {member_name}".format(
+            title=self.title,
+            member_name="\n  - ".join([m.name for m in self.member]),
+        )
+
+
+if __name__ == "__main__":
+    raara = Idol(name="raara", profile="kashikoma Idol")
+    mirei = Idol(name="mirei", profile="pripri Idol")
+    today_live = Live(title="Bold SUMMER ADVENTURE", member=[raara, mirei])
+    print(today_live.summary())
