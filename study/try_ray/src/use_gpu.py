87350b0fb80bccabb92b05fbe84ca389fd2f2b71
yumechi <yumechi0525@gmail.com>
2020-12-23 12:14:02 +0900

    use gpu

---
+++ study/try_ray/src/use_gpu.py
@@ -0,0 +1,12 @@
+import ray
+import os
+
+ray.init(num_gpus=1)
+
+@ray.remote(num_gpus=1)
+def use_gpu():
+    print("ray.get_gpu_ids(): {}".format(ray.get_gpu_ids()))
+    print("CUDA_VISIBLE_DEVICES: {}".format(os.environ["CUDA_VISIBLE_DEVICES"]))
+
+use_gpu.remote()
+
