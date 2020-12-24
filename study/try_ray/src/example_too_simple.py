870e3307f3c57253d8d387c064ca65b1bc2f548c
yumechi <yumechi0525@gmail.com>
2020-12-24 22:26:23 +0900

    ray sample

--- study/try_ray/src/example_too_simple.py
+++ study/try_ray/src/example_too_simple.py
@@ -1,14 +1,13 @@
 import ray
 import os
 
-ray.init(num_gpus=1)
+ray.init()
 
-@ray.remote(num_gpus=0.33)
+@ray.remote
 def f(x):
     print("ray.get_gpu_ids(): {}".format(ray.get_gpu_ids()))
     print("CUDA_VISIBLE_DEVICES: {}".format(os.environ["CUDA_VISIBLE_DEVICES"]))
     return x * x
 
-for j in range(100):
-    futures = [f.remote(i) for i in range(j ** 2)]
-    print(j, ray.get(futures))
+futures = [f.remote(i) for i in range(100)]
+print(ray.get(futures))
