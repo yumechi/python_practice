c10af3ccf8bf68a7a54bf1e0822126058b03f95f
yumechi <yumechi0525@gmail.com>
2021-01-13 06:20:43 +0900

    query parameter

--- study/try_fastapi/main.py
+++ study/try_fastapi/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -7,6 +8,13 @@ class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"},
+]
 
 
 app = FastAPI()
@@ -28,9 +36,46 @@ async def read_user_me(user_id: str):
     return {"user_id": user_id}
 
 
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# query_params の bool は扱いが少し特殊
+# Trueになる 1, True, true, on, yes（大文字小文字問わない）
+# Noになる 0, False, false, off, no（実際は True にならないすべての any なパターン）
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_item(
+    item_id: int, q: Optional[str] = None, short: bool = False
+):
+    items = {"item_id": item_id}
+    if q:
+        items |= {"q": q}
+    if not short:
+        items |= {
+            "description": "This is an amazing item that has a long description"
+        }
+    return items
+
+
+@app.get("/items_2/{item_id}")
+async def read_user_item_needy(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Optional[str] = None, short: bool = False
+):
+    items = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        items |= {"q": q}
+    if not short:
+        items |= {
+            "description": "This is an amazing item that has a long description"
+        }
+    return items
 
 
 @app.get("/models/{model_name}")
