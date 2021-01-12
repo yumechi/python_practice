d308acc25681166cede156852ada2dd8028e86f4
yumechi <yumechi0525@gmail.com>
2021-01-13 05:55:36 +0900

    Path priority

--- study/try_fastapi/main.py
+++ study/try_fastapi/main.py
@@ -2,20 +2,36 @@ from enum import Enum
 
 from fastapi import FastAPI
 
+
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+# 先に宣言しないと `/user/{user_id}` 側に吸収されるため、宣言順序が大事
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user_me(user_id: str):
+    return {"user_id": user_id}
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
